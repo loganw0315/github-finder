f6d01cb9c6a924c9e58a3e1159fd0b0f235cd3e9
Logan Willis <logan.willis@perficient.com>
2022-03-24 16:15:03 -0500

    First commit

---
+++ src/components/users/UserResults.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useEffect, useContext } from 'react'
+import Spinner from '../layout/Spinner'
+import UserItem from './UserItem'
+import GithubContext from '../../context/github/GithubContext'
+
+function UserResults() {
+    const {fetchUsers, loading, users} = useContext(GithubContext)
+
+    useEffect(() => {
+        fetchUsers()
+    },[])
+
+    if(!loading){
+        return (
+            <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>{users.map((user) => (
+                <h3><UserItem key={user.id} user={user}/></h3>
+            ))}</div>
+          )
+    } else {
+        return <h3><Spinner/></h3>
+    }
+}
+
+export default UserResults   
