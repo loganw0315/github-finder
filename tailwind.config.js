f69a2a3b2231f9496843000604b304b0ce894447
Logan Willis <logan.willis@perficient.com>
2022-03-28 16:15:51 -0500

    Final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  themes: ["dark"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
   plugins: [require('daisyui')],
 }
